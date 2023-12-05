1f4d00c89e45b6c2fd6222c2991d386ca972ec41
krysnl <korayemresnl@gmail.com>
2023-12-06 01:28:24 +0300

    day 3 part 2 solution
    its dirty

--- day3.js
+++ day3.js
@@ -1,6 +1,6 @@
 const { input } = require("./input");
 
-let anw = 0;
+let ans = 0;
 let num = "";
 let flag = false;
 
@@ -63,19 +63,19 @@ const isValidCoord = (x, y) => {
 
 for (let i = 0; i < input.length; i++) {
     if (flag) {
-        anw += parseInt(num);
+        ans += parseInt(num);
     }
     num = "";
     flag = false;
     for (let j = 0; j < input[i].length; j++) {
         if (!isNaN(input[i][j])) {
-            num = num + "" + input[i][j];
+            num = num + input[i][j];
             if (checkAdjacent(i, j)) {
                 flag = true;
             }
         } else {
             if (flag) {
-                anw += parseInt(num);
+                ans += parseInt(num);
             }
             num = "";
             flag = false;
@@ -83,7 +83,72 @@ for (let i = 0; i < input.length; i++) {
     }
 }
 if (flag) {
-    anw += parseInt(num);
+    ans += parseInt(num);
 }
 
-console.log(anw);
+console.log(ans);
+
+// Part 2
+const checkAdjacentStar = (x, y) => {
+    if (isValidCoord(x - 1, y - 1) && input[x - 1][y - 1] === "*") {
+        return x - 1 + " " + (y - 1);
+    } else if (isValidCoord(x - 1, y) && input[x - 1][y] === "*") {
+        return x - 1 + " " + y;
+    } else if (isValidCoord(x - 1, y + 1) && input[x - 1][y + 1] === "*") {
+        return x - 1 + " " + (y + 1);
+    } else if (isValidCoord(x, y - 1) && input[x][y - 1] === "*") {
+        return x + " " + (y - 1);
+    } else if (isValidCoord(x, y + 1) && input[x][y + 1] === "*") {
+        return x + " " + (y + 1);
+    } else if (isValidCoord(x + 1, y - 1) && input[x + 1][y - 1] === "*") {
+        return x + 1 + " " + (y - 1);
+    } else if (isValidCoord(x + 1, y) && input[x + 1][y] === "*") {
+        return x + 1 + " " + y;
+    } else if (isValidCoord(x + 1, y + 1) && input[x + 1][y + 1] === "*") {
+        return x + 1 + " " + (y + 1);
+    }
+    return false;
+};
+
+const starMatSetter = (star, num) => {
+    starMap.get(star)
+        ? starMap.set(star, [parseInt(num), ...starMap.get(star)])
+        : starMap.set(star, [parseInt(num)]);
+};
+
+ans = 0;
+const starMap = new Map();
+let star = "";
+for (let i = 0; i < input.length; i++) {
+    if (flag) {
+        starMatSetter(star, num);
+    }
+    num = "";
+    flag = false;
+    for (let j = 0; j < input[i].length; j++) {
+        if (!isNaN(input[i][j])) {
+            num = num + input[i][j];
+            if (checkAdjacentStar(i, j)) {
+                star = checkAdjacentStar(i, j);
+                flag = true;
+            }
+        } else {
+            if (flag) {
+                starMatSetter(star, num);
+            }
+            num = "";
+            flag = false;
+        }
+    }
+}
+if (flag) {
+    starMatSetter(star, num);
+}
+
+starMap.forEach((value) => {
+    if (value.length === 2) {
+        ans += value[0] * value[1];
+    }
+});
+
+console.log(ans);
