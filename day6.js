d4a14fbb7999304ad49a570f33e2e6ee522ffcf2
krysnl <korayemresnl@gmail.com>
2023-12-11 16:55:40 +0300

    day 6 part 2 solution.
    not the best solution but ...

--- day6.js
+++ day6.js
@@ -28,3 +28,29 @@ for (let i = 0; i < times.length; i++) {
     ans = ans * getOptions(time, distance);
 }
 console.log(ans);
+
+// Part 2
+
+const time = input[0].split(": ")[1].replaceAll(" ", "");
+
+const distance = input[1].split(": ")[1].replaceAll(" ", "");
+
+console.log(time, distance);
+
+const getOption = (time, distance) => {
+    let options = 0;
+    let flag = false;
+    for (let i = 0; i < time; i++) {
+        if (i * (time - i) > distance) {
+            flag = true;
+            options++;
+            continue;
+        }
+        if (flag) {
+            break;
+        }
+    }
+    return options;
+};
+
+console.log(getOption(time, distance));
